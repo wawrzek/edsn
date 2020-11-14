857f049cdaf4885570c424174188eae6f0f0ecfa
Wawrzyniec 'Wawrzek' Niewodniczański <main@wawrzek.name>
2020-11-14 12:48:20 +0000

    Tests: Error status on exit

--- tests/simple_tests.py
+++ tests/simple_tests.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-import subprocess
 import hashlib
+import subprocess
+import sys
 
 
 hashes = {
@@ -9,8 +10,10 @@ hashes = {
     "v1688aquilae-r15.json": "41ef7be79b300f96dca5c12ed981bf560229107a"
         }
 
+errors = 0
 
 def compare_hashes(filename):
+    errors_hashes = 0
     with open(filename) as f:
         text = f.read().encode('utf-8')
         h = hashlib.sha1(text).hexdigest()
@@ -19,12 +22,17 @@ def compare_hashes(filename):
         else:
             print ('ERROR!')
             print (f'SHA1 hash for {filename} is wrong')
+            errors_hashes = 1
+        return errors_hashes
 
 
 print ("Test script with default values")
 command1 = subprocess.run('../prepare_data.py', capture_output=True, check=True)
-compare_hashes('v1688aquilae-r15.json')
+errors += compare_hashes('v1688aquilae-r15.json')
 
 print ("Test script with system=Sol, radius=20")
 command2 = subprocess.run(['../prepare_data.py', '-s', 'Sol', '-r', '20'], capture_output=True, check=True)
-compare_hashes('sol-r20.json')
+errors += compare_hashes('sol-r20.json')
+
+if errors > 0:
+    sys.exit(1)
